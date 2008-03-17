afa9eb491cc3ea19a1a85b721a30cfb880eb38d9
jens.schwanke <jens.schwanke@229a4498-4a46-0410-8831-316b8a3adfa8>
2008-03-17 16:48:27 +0000

    Projekt Start

---
+++ trunk/CTPV/CTViewer/View/MainView.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CTViewer.View
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
